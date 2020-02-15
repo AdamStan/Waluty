7e727893406466500eccdb3c187c8d4ae9c0b4c5
AdamStan <adam15stan@gmail.com>
2020-02-15 12:49:38 +0100

    initial commit

---
+++ ExchangeRates/Cash.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeRates
+{
+    /**
+     * Class for Currency parameters, more at http://api.nbp.pl
+     */
+    class Cash
+    {
+        /** Name */
+        public string Currency { get; set; }
+        public string Country { get; set; }
+        public DateTime EffectiveDate { get; set; }
+        /** przeliczony kurs kupna waluty */
+        public long Bid { get; set; }
+        /** przeliczony kurs sprzedaży waluty */
+        public long Ask { get; set; }
+        /** przeliczony kurs średni waluty */
+        public long Mid { get; set; }
+    }
+}
